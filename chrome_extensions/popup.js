57627fb14cb557eb2c3cff8816040f07621b2873
kajzuang <zuangjason@gmail.com>
2016-04-16 15:58:25 -0500

    here

--- chrome_extensions/popup.js
+++ chrome_extensions/popup.js
@@ -1,10 +1,22 @@
 var count = 1;
-function myFunctionLoad()
+document.addEventListener('DOMContentLoaded',function()
 {
-		var slot = document.getElementByID("myTable");
-		var header = table.createHead();
+	var loadButt = document.getElementById('load');
+	loadButt.addEventListener('click',function()
+	{
+		var slot = document.getElementById("myTable");
+		var header = slot.createTHead();
 		var row = header.insertRow(0);
 		var cell = row.insertCell(0);
 		cell.innerHTML = "Course " +count;
 		count = count+1;
-}
+	});
+});
+	
+		/*var slot = document.getElementByID("myTable");
+		var header = table.createHead();
+		var row = header.insertRow(0);
+		var cell = row.insertCell(0);
+		cell.innerHTML = "Course " +count;
+		count = count+1;*/
+
