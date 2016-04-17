ba6b54fdef0c3e28aa02b8b03c51bb9b4e523d9d
kajzuang <zuangjason@gmail.com>
2016-04-16 23:53:21 -0500

    fds

--- chrome_extensions/popup.js
+++ chrome_extensions/popup.js
@@ -1,22 +1,112 @@
+var loadButt = document.getElementById('load');
 var count = 1;
-document.addEventListener('DOMContentLoaded',function()
+var courses =[];
+if(loadButt)
 {
-	var loadButt = document.getElementById('load');
+	loadButt.addEventListener('click',function(global)
+	{
+		if(win)
+		{
+		}
+		else
+		{
+			var win = window.open('tables.html','MyTables');
+		}
+
+		localStorage.setItem("Courses","courses" + count);
+		count += 1;
+		console.log("hi");
+
+	/*var slot = document.getElementById('myTable');	
+	var header = slot.createTHead();
+	var row = header.insertRow(0);
+	var cell = row.insertCell(0);
+	var a = "Course"+count;
+	courses.push(a);
+	cell.innerHTML = a;
+	count = count+1;*/
+	});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var bgPage = chrome.extension.getBackgroundPage();
+var courses = [];
+var count = 1;
+var loadButt = document.getElementById('load');
+var deleteButt = document.getElementById('delete');
+var storeButt = document.getElementById('store');
+var slot = document.getElementById("myTable");
+/*window.addEventListener("unload",function(event)
+{
+	chrome.extension.getBackgroundPage().docBody = document.body;
+
+});
+window.addEventListener("load",function(event)
+{
+	var docBody = document.body;
+	var lastDocBody = bgPage.docBody;
+	if(lastDocBody)
+	{
+		docBody.parentElement.replaceChild(document.importNode(lastDocBody,true),docBody);
+
+	}
+});
+window.addEventListener("unload",function(event)
+{
+	alert("im unloading");
+
+});
+window.addEventListener("load",function(event)
+{
+
+});
+if(loadButt)
+{
 	loadButt.addEventListener('click',function()
 	{
-		var slot = document.getElementById("myTable");
-		var header = slot.createTHead();
-		var row = header.insertRow(0);
-		var cell = row.insertCell(0);
-		cell.innerHTML = "Course " +count;
-		count = count+1;
+    
+		chrome.tabs.create({'url': chrome.extension.getURL('popup.html')}, function(tab) {});
+
+
+	var header = slot.createTHead();
+	var row = header.insertRow(0);
+	var cell = row.insertCell(0);
+	var a = "Course"+count;
+	courses.push(a);
+	cell.innerHTML = a;
+	count = count+1;
 	});
-});
-	
-		/*var slot = document.getElementByID("myTable");
-		var header = table.createHead();
-		var row = header.insertRow(0);
-		var cell = row.insertCell(0);
-		cell.innerHTML = "Course " +count;
-		count = count+1;*/
+}
+if(storeButt)
+{
+	storeButt.addEventListener("click",function()
+	{
+	/*var slot = document.getElementById("myTable");
+	slot.innerHTML = "";
+	});
+	for(i=0;i<courses.length;i++)
+	{
+		console.log(courses[i]);
+	}
+	});
+}
+*/
+
 
